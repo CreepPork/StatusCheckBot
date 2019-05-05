adcef5f4592608ce6f2024a59a86a7f84c5a85fc
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-05-06 00:43:21 +0300

    Added not working label and fixed on new commit, fixed #2 and fixed #3

--- src/bot.ts
+++ src/bot.ts
@@ -9,17 +9,20 @@ if (! process.env.APP_ID ||
     ! process.env.WEBHOOK_SECRET ||
     ! process.env.WEBHOOK_PROXY_URL ||
     (! process.env.PRIVATE_KEY_PATH && ! process.env.PRIVATE_KEY) ||
-    ! process.env.LABEL) {
+    ! process.env.LABEL || ! process.env.LABEL_NOT_WORKING) {
         throw new Error(`Check your .env file as required data hasn't been filled.`);
 }
 
 const title = process.env.GITHUB_STATUS_TITLE || 'Is tested?';
 const bodySuccess = process.env.GITHUB_STATUS_BODY_SUCCESS || 'This pull request has been tested in-game.';
+const bodyNotWorking = process.env.GITHUB_STATUS_BODY_NOT_WORKING ||
+    'This pull request has been tested in-game but is not working as desired.';
 const bodyFail = process.env.GITHUB_STATUS_BODY_FAILURE || `This pull request hasn't been tested in-game.`;
 
 module.exports = (app: probot.Application) => {
-    app.on(['pull_request.labeled', 'pull_request.unlabeled'], async context => {
+    app.on(['pull_request.labeled', 'pull_request.unlabeled', 'pull_request.synchronize'], async context => {
         let foundLabel = false;
+        let foundNotWorkingLabel = false;
 
         const payload: IPullRequestEvent = context.payload;
 
@@ -28,6 +31,10 @@ module.exports = (app: probot.Application) => {
             labels.forEach(label => {
                 if (label.name === process.env.LABEL) {
                     foundLabel = true;
+                }
+
+                if (label.name === process.env.LABEL_NOT_WORKING) {
+                    foundNotWorkingLabel = true;
                 }
             });
         }
@@ -49,7 +56,7 @@ module.exports = (app: probot.Application) => {
         } else {
             context.github.repos.createStatus({
                 context: title,
-                description: bodyFail,
+                description: foundNotWorkingLabel ? bodyNotWorking : bodyFail,
                 owner: context.issue().owner,
                 repo: context.issue().repo,
                 sha: payload.pull_request.head.sha,
